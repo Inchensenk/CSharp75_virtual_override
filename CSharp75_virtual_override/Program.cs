c76733468a4430fb667ef08cfb17b2c80a83b0f0
sofoyan gevork <aab81@bk.ru>
2022-06-02 19:13:54 +0300

    Добавьте файлы проекта.

---
+++ CSharp75_virtual_override/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp75_virtual_override
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
